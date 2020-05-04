a524ae0362cde63f9320325701d11ad8f0e2a298
Zhao Wang <wangz2266@gmail.com>
2020-05-04 11:15:46 -0700

    add input and ref

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
+import User from "../interfaces/User.interface";
 
-export class PokemonSearch extends Component<{
-  name: string;
-  numberOfPokemons: number;
-}> {
+export class PokemonSearch extends Component<User> {
+  pokemonRef: React.RefObject<HTMLInputElement>;
+  constructor(props: User) {
+    super(props);
+    this.pokemonRef = React.createRef();
+  }
   render() {
     const { name, numberOfPokemons } = this.props;
     return (
       <div>
         <p>
-          User {name} has {numberOfPokemons} pokemons.
+          User {name}{" "}
+          {numberOfPokemons && <span> has {numberOfPokemons} pokemons. </span>}
         </p>
+        <input type="text" ref={this.pokemonRef} />
+        <button onClick={this.onSearchClick} className="my-button">
+          Search!
+        </button>
       </div>
     );
   }
