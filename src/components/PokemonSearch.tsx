35bfc57f8a66fcfea5d05ef32e399223a59f3680
Zhao Wang <wangz2266@gmail.com>
2020-05-04 11:45:37 -0700

    add search logic and show result

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -3,6 +3,10 @@ import User from "../interfaces/User.interface";
 
 interface SearchState {
   error: boolean;
+  pokemon: Pokemon;
+}
+
+interface Pokemon {
   name: string;
   numberOfAbilities: number;
   baseExperience: number;
@@ -17,10 +21,7 @@ export class PokemonSearch extends Component<User, SearchState> {
     this.pokemonRef = React.createRef();
     this.state = {
       error: false,
-      name: "",
-      numberOfAbilities: 0,
-      baseExperience: 0,
-      imageUrl: "",
+      pokemon: null,
     };
   }
 
@@ -34,27 +35,47 @@ export class PokemonSearch extends Component<User, SearchState> {
       res.json().then((data) => {
         this.setState({
           error: false,
-          name: data.name,
-          numberOfAbilities: data.abilities.length,
-          baseExperience: data.base_experience,
-          imageUrl: data.sprites.front_default,
+          pokemon: {
+            name: data.name,
+            numberOfAbilities: data.abilities.length,
+            baseExperience: data.base_experience,
+            imageUrl: data.sprites.front_default,
+          },
         });
       });
     });
   };
 
   render() {
-    const { name, numberOfPokemons } = this.props;
+    const { name: username, numberOfPokemons } = this.props;
+    const { error, pokemon } = this.state;
+    let resultMarkup;
+
+    if (error) {
+      resultMarkup = <p>Pokemon not found, please try again.</p>;
+    } else if (this.state.pokemon) {
+      resultMarkup = (
+        <div>
+          <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
+          <p>
+            {pokemon.name} has {pokemon.numberOfAbilities} abilities and{" "}
+            {pokemon.baseExperience} base experience points.
+          </p>
+        </div>
+      );
+    }
+
     return (
       <div>
         <p>
-          User {name}{" "}
+          User {username}{" "}
           {numberOfPokemons && <span> has {numberOfPokemons} pokemons. </span>}
         </p>
         <input type="text" ref={this.pokemonRef} />
         <button onClick={this.onSearchClick} className="my-button">
           Search!
         </button>
+        {resultMarkup}
       </div>
     );
   }
